7ae3e710671485ef277b0ca02e8c94b080e82bc2
Control <control8205@gmail.com>
2023-05-14 01:03:47 +0800

    01初始化

---
+++ 01-cesium入门/06-添加地形与添加自定义地形/cesium-basic/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+ <div id="cesiumContainer"></div>
+</template>
+
+<script setup>
+  import * as Cesium from "cesium"
+  import "./Widgets/widgets.css"
+  import { onMounted } from "vue";
+
+  window.CESIUM_BASE_URL = "/";
+
+  onMounted(() => {
+    let viewer = new Cesium.Viewer("cesiumContainer")
+    console.log(viewer)
+  });
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#cesiumContainer {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
